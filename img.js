004fe0de86a1c2a029804e1734e54aad5325693b
MaidenAngles <154468876+MaidenAngles@users.noreply.github.com>
2023-12-20 23:20:17 -0800

    Add files via upload

---
+++ img.js
@@ -0,0 +1,16 @@
+const Img = [];
+const btn = document.getElementById("btn");
+const Imgs = document.querySelector(".Img");
+
+btn.addEventListener("click", function () {
+  const randomImg = getRandomNumber();
+  // console.log(randomNumber);
+
+ 
+  document.body.style.backgroundImage = Img[randomImg];
+  Imgs.textContent = Imgs[randomImg];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * Img.length);
+}
